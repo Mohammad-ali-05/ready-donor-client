1c90b21fce4337b900deba8ef4fd6dfd8d6b15bd
Mohammad Ali <muhammadali.pass1@gmail.com>
2026-05-31 06:28:04 +0600

    Completed MyDonationRequest and all its functionality except the action buttons.

--- src/pages/dashboard/myDonationRequest/MyDonationRequest.jsx
+++ src/pages/dashboard/myDonationRequest/MyDonationRequest.jsx
@@ -1,9 +1,286 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Loading from "../../../components/Loading";
+
+// Filter options
+const statuses = ["all", "pending", "inprogress", "done", "canceled"];
 
 const MyDonationRequest = () => {
+    // React hooks
+    const [donationRequests, setDonationRequests] = useState([]);
+    const [loadingData, setLoadingData] = useState(true);
+
+    // States for pagination
+    const [donationRequestCount, setRequestDonationCount] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    const limit = 10;
+
+    // State for filter
+    const [statusFilter, setStatusFilter] = useState("all");
+
+    /* Custom hooks */
+    const axiosSecure = useAxiosSecure();
+
+    // fetch donation requests
+    useEffect(() => {
+        const fetchDonationRequests = async () => {
+            try {
+                setLoadingData(true);
+                const result = await axiosSecure.get("/blood-donation", {
+                    params: {
+                        limit,
+                        skip: currentPage * limit,
+                        statusFilter,
+                    },
+                });
+
+                const count = result.data.donationCount;
+
+                console.log(result.data);
+                setDonationRequests(result.data.donations);
+                setRequestDonationCount(count);
+                setTotalPage(Math.ceil(count / limit));
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoadingData(false);
+            }
+        };
+        fetchDonationRequests();
+    }, [axiosSecure, currentPage, statusFilter]);
+
+    //  Convert date to 12 hour formate
+    const formatTime = (time24) => {
+        const [hourStr, minute] = time24.split(":");
+        let hour = parseInt(hourStr, 10);
+
+        const ampm = hour >= 12 ? "PM" : "AM";
+        hour = hour % 12;
+        if (hour === 0) hour = 12;
+
+        return `${hour}:${minute} ${ampm}`;
+    };
+
     return (
-        <div>
-            
+        <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold mb-4">
+                All Donation Requests{" "}
+                <span className="bg-linear-to-br from-[#B32346] to-[#46052D] bg-clip-text text-transparent">
+                    {donationRequestCount}
+                </span>{" "}
+                🩸
+            </h2>{" "}
+            {/* Filter Tabs */}
+            <div className="flex gap-2 mb-4 flex-wrap">
+                {statuses.map((status) => (
+                    <button
+                        key={status}
+                        onClick={() => {
+                            setStatusFilter(status);
+                            setCurrentPage(0);
+                        }}
+                        className={`px-4 py-2 rounded-md border text-sm ${
+                            statusFilter === status
+                                ? "text-white bg-linear-to-r from-[#B32346] to-[#46052D] hover:opacity-85"
+                                : "hover:text-white hover:bg-linear-to-r hover:from-[#B32346] hover:to-[#46052D]"
+                        }`}>
+                        {status}
+                    </button>
+                ))}
+            </div>
+            {loadingData ? (
+                <div className="flex justify-center items-center my-20">
+                    <Loading></Loading>
+                </div>
+            ) : (
+                <>
+                    <div className="overflow-x-auto">
+                        <table className="w-full text-sm">
+                            <thead className="bg-gray-100 text-left">
+                                <tr>
+                                    <th className="p-3">Recipient</th>
+                                    <th className="p-3">Location</th>
+                                    <th className="p-3">Date</th>
+                                    <th className="p-3">Time</th>
+                                    <th className="p-3">Blood</th>
+                                    <th className="p-3">Status</th>
+                                    <th className="p-3">Donor</th>
+                                    <th className="p-3">Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {donationRequests.map((req) => (
+                                    <tr key={req._id} className="border-t">
+                                        <td className="p-3">
+                                            {req.recipientName}
+                                        </td>
+
+                                        <td className="p-3">
+                                            {req.districtName},{" "}
+                                            {req.upazilaName}
+                                        </td>
+
+                                        <td className="p-3">
+                                            {req.donationDate
+                                                .split("-")
+                                                .join("/")}
+                                        </td>
+                                        <td className="p-3">
+                                            {formatTime(req?.donationTime)}
+                                        </td>
+                                        <td className="p-3 font-semibold">
+                                            {req.bloodGroup}
+                                        </td>
+
+                                        {/* Status */}
+                                        <td className="p-3">
+                                            <span
+                                                className={`px-2 py-1 text-xs rounded ${
+                                                    req.status === "pending"
+                                                        ? "bg-yellow-100 text-yellow-700"
+                                                        : req.status ===
+                                                            "inprogress"
+                                                          ? "bg-blue-100 text-blue-700"
+                                                          : req.status ===
+                                                              "done"
+                                                            ? "bg-green-100 text-green-700"
+                                                            : "bg-red-100 text-red-700"
+                                                }`}>
+                                                {req.status}
+                                            </span>
+                                        </td>
+
+                                        {/* donor info only if inprogress */}
+                                        <td className="p-3">
+                                            {req.status === "inprogress" ||
+                                            req.status === "done" ? (
+                                                <div className="text-xs text-gray-500 mt-1">
+                                                    {req.donorName} (
+                                                    {req.donorEmail})
+                                                </div>
+                                            ) : (
+                                                <div className="text-xs text-gray-500 mt-1">
+                                                    {"No donor found"}
+                                                </div>
+                                            )}
+                                        </td>
+
+                                        {/* Actions */}
+                                        <td className="p-3 grid grid-cols-1 lg:grid-cols-3 gap-2">
+                                            {/* View */}
+                                            <button
+                                                onClick={() =>
+                                                    `/dashboard/donation/${req._id}`
+                                                }
+                                                className="px-2 py-1 w-full text-xs bg-gray-100 rounded">
+                                                View
+                                            </button>
+
+                                            {/* Edit and delete button when request pending */}
+                                            {req.status === "pending" && (
+                                                <>
+                                                    {/* Edit */}
+                                                    <Link
+                                                        to={`/dashboard/my-donation-requests/edit/${req._id}`}>
+                                                        <button className="px-2 w-full py-1 text-xs bg-blue-100 text-blue-700 rounded">
+                                                            Edit
+                                                        </button>
+                                                    </Link>
+                                                    {/* Delete */}
+                                                    <button
+                                                        onClick={() =>
+                                                            handleDelete(
+                                                                req._id,
+                                                            )
+                                                        }
+                                                        className="px-2 py-1 w-full text-xs bg-red-100 text-red-700 rounded">
+                                                        Delete
+                                                    </button>
+                                                </>
+                                            )}
+
+                                            {/* Done or cancel button when request inprogress */}
+                                            {req.status === "inprogress" && (
+                                                <>
+                                                    <button
+                                                        onClick={() =>
+                                                            updateStatus(
+                                                                req._id,
+                                                                "done",
+                                                            )
+                                                        }
+                                                        className="px-2 py-1 w-full text-xs bg-green-100 text-green-700 rounded">
+                                                        Done
+                                                    </button>
+
+                                                    <button
+                                                        onClick={() =>
+                                                            updateStatus(
+                                                                req._id,
+                                                                "canceled",
+                                                            )
+                                                        }
+                                                        className="px-2 py-1 w-full text-xs bg-red-100 text-red-700 rounded">
+                                                        Cancel
+                                                    </button>
+                                                </>
+                                            )}
+                                        </td>
+                                    </tr>
+                                ))}
+                                {donationRequestCount === 0 && (
+                                    <tr>
+                                        <td
+                                            colSpan={"8"}
+                                            className="text-center py-20 text-gray-500">
+                                            No donation requests found
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {/* If donation course is lower the limit then hide pagination */}
+                    {donationRequestCount < limit || (
+                        <div className="flex justify-center flex-wrap gap-3 py-10">
+                            {currentPage > 0 && (
+                                <button
+                                    onClick={() =>
+                                        setCurrentPage(currentPage - 1)
+                                    }
+                                    className="btn hover:text-white hover:bg-linear-to-r hover:from-[#B32346] hover:to-[#46052D]">
+                                    Prev
+                                </button>
+                            )}
+                            {[
+                                ...Array(totalPage)
+                                    .keys()
+                                    .map((i) => (
+                                        <button
+                                            onClick={() => setCurrentPage(i)}
+                                            key={i}
+                                            className={`btn rounded-md ${currentPage === i && "text-white bg-linear-to-r from-[#B32346] to-[#46052D] hover:opacity-85"}`}>
+                                            {i + 1}
+                                        </button>
+                                    )),
+                            ]}
+                            {currentPage + 1 < totalPage && (
+                                <button
+                                    onClick={() =>
+                                        setCurrentPage(currentPage + 1)
+                                    }
+                                    className="btn hover:text-white hover:bg-linear-to-r hover:from-[#B32346] hover:to-[#46052D]">
+                                    Next
+                                </button>
+                            )}
+                        </div>
+                    )}
+                </>
+            )}
         </div>
     );
 };
