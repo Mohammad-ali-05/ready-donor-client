a6ea49552addccb0b994ee64884c1ef0d94a059d
Mohammad Ali <muhammadali.pass1@gmail.com>
2026-06-04 09:57:46 +0600

    Added pagination to BloodDonations.

--- src/pages/bloodDonations/BloodDonations.jsx
+++ src/pages/bloodDonations/BloodDonations.jsx
@@ -9,6 +9,12 @@ const BloodDonations = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [donorCount, setDonorCount] = useState(0);
 
+    // States for pagination
+    const [donationRequestCount, setDonationRequestCount] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    const limit = 9;
+
     // Custom hooks
     const axios = useAxios();
 
@@ -17,10 +23,19 @@ const BloodDonations = () => {
             try {
                 setIsLoading(true);
 
-                const donations = await axios.get("/pending-blood-donations");
+                const donations = await axios.get("/pending-blood-donations", {
+                    params: {
+                        limit,
+                        skip: currentPage * limit,
+                    },
+                });
+
+                const count = donations.data.donationsCount;
 
                 setAllDonations(donations.data.donations);
                 setDonorCount(donations.data.donationsCount);
+                setDonationRequestCount(count);
+                setTotalPage(Math.ceil(count / limit));
             } catch (error) {
                 console.error("Failed to fetch donations:", error);
             } finally {
@@ -29,7 +44,7 @@ const BloodDonations = () => {
         };
 
         fetchDonations();
-    }, [axios]);
+    }, [axios, currentPage]);
 
     return (
         <section className="min-h-screen bg-gray-50 py-12">
@@ -44,21 +59,57 @@ const BloodDonations = () => {
                     </p>
                 </div>
             </div>
-            {isLoading ? (
-                <div className="bg-white flex justify-center items-center shadow-sm rounded-xl max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-55 w-full">
-                    <Loading></Loading>
-                </div>
-            ) : donorCount ? (
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                    {allDonations?.map((donation) => (
-                        <DonationCard key={donation._id} donation={donation} />
-                    ))}
-                </div>
-            ) : (
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
-                    <p className="bg-white rounded-xl shadow-sm p-6 mb-8 text-center py-30 text-gray-500">
-                        No donation requests found
-                    </p>
+            <>
+                {isLoading ? (
+                    <div className="bg-white flex justify-center items-center shadow-sm rounded-xl max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-55 w-full">
+                        <Loading></Loading>
+                    </div>
+                ) : donorCount ? (
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {allDonations?.map((donation) => (
+                            <DonationCard
+                                key={donation._id}
+                                donation={donation}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
+                        <p className="bg-white rounded-xl shadow-sm p-6 mb-8 text-center py-30 text-gray-500">
+                            No donation requests found
+                        </p>
+                    </div>
+                )}
+            </>
+            {/* If donation count is lower then the limit, hide pagination */}
+            {donationRequestCount <= limit || (
+                <div className="flex justify-center flex-wrap gap-3 py-10">
+                    {currentPage > 0 && (
+                        <button
+                            onClick={() => setCurrentPage(currentPage - 1)}
+                            className="btn hover:text-white hover:bg-linear-to-r hover:from-[#B32346] hover:to-[#46052D]">
+                            Prev
+                        </button>
+                    )}
+                    {[
+                        ...Array(totalPage)
+                            .keys()
+                            .map((i) => (
+                                <button
+                                    onClick={() => setCurrentPage(i)}
+                                    key={i}
+                                    className={`btn rounded-md ${currentPage === i && "text-white bg-linear-to-r from-[#B32346] to-[#46052D] hover:opacity-85"}`}>
+                                    {i + 1}
+                                </button>
+                            )),
+                    ]}
+                    {currentPage + 1 < totalPage && (
+                        <button
+                            onClick={() => setCurrentPage(currentPage + 1)}
+                            className="btn hover:text-white hover:bg-linear-to-r hover:from-[#B32346] hover:to-[#46052D]">
+                            Next
+                        </button>
+                    )}
                 </div>
             )}
         </section>
